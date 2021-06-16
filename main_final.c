56e58590ad20251a6a55ccbb1f225c3f8bd34221
kareemowis413 <67554868+kareemowis413@users.noreply.github.com>
2021-06-16 16:46:13 +0200

    Update main_final.c

--- main_final.c
+++ main_final.c
@@ -8,9 +8,13 @@ void led_on(long double dist)
 
 int main(){
 
-initialize(); // call the initialization finction
+initialize();    //Call the initialization of the GPIO of tiva
+
 initalizeLCD();
-init_uart();
+/*Call the intialization commands, data length set, then clear the lcd screen 
+of any previous data, return home command, and turning the display on + controlling the cursor*/ 
+
+init_uart(); //uart ports initializations' 
 
 
 
@@ -18,9 +22,10 @@ init_uart();
 
 GPIO_PORTF_DATA_R &= ~0x02;
 dist = 0.0;
-
-printOnLCD(Start0, 1);
-printOnLCD(Start1, 2);
+        
+/*print on the lcd the message to print for the gps programe to start by using SW2*/
+printOnLCD(Start0, 1); //press SW2 to, 1stROW
+printOnLCD(Start1, 2);//Start 2ndROW
 
 while((GPIO_PORTF_DATA_R & 0x10)){
 
@@ -30,12 +35,13 @@ printOnLCD(Stop0, 1);
 printOnLCD(Stop1, 2);
 
 delay10Ns(10000000);     //delay 10 seconds
+/*printing on lcd the word "Distance= " and the calculated distance right after it. AT 1stROW*/
 printOnLCD(Distance, 1);
 printDist(dist);
 
 while((GPIO_PORTF_DATA_R & 0x10)){
 
-
+/* o is a variable set to idintefy if the recived data from the gps are correct, after reseting the value of O to 0 and then re-check*/
         lat_long();
             if (o == 1)
             {
@@ -43,7 +49,7 @@ while((GPIO_PORTF_DATA_R & 0x10)){
             }
             o = 0 ;
       printDist(dist);
-      led_on(dist);
+      led_on(dist); //red led on after dist value reachs 100meter
       }
       printOnLCD(Stop2, 2);
       delay10Ns(1000000);			//delay 10 seconds
